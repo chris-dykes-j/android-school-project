214351acebd454e4f9d072cceb5fbbdef9a24c8a
kim00451 <kim00451@algonquinlive.com>
2022-07-22 18:20:44 -0400

    Taeyeon test

--- app/src/main/java/com/cst2335/ticketmaster/Tycart.java
+++ app/src/main/java/com/cst2335/ticketmaster/Tycart.java
@@ -10,5 +10,6 @@ public class Tycart extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tycart);
+        //test
     }
 }
