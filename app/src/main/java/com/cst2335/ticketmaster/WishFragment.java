b3ed9ff10fa2c6acfdf562a3c2841018cc742a6f
Jeong Mi Park <parkjeongmi0227@gmail.com>
2022-07-28 19:18:08 -0400

    added fragment into wishlist

---
+++ app/src/main/java/com/cst2335/ticketmaster/WishFragment.java
@@ -0,0 +1,62 @@
+package com.cst2335.ticketmaster;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class WishFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String MYSUBNAME = "mySubName";
+    private static final String MYSUBIMAGE = "mySubImage";
+
+    // TODO: Rename and change types of parameters
+    private String mySubName;
+    private String mySubImage;
+    private ImageView imageView;
+    private TextView textView;
+
+    public WishFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mySubName = getArguments().getString(MYSUBNAME);
+            mySubImage = getArguments().getString(MYSUBIMAGE);
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_my_wish_fragment, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
+        textView = (TextView) view.findViewById(R.id.mySubName);
+        textView.setText(mySubName);
+
+        Log.i("mySubName : ", mySubName);
+        Log.i("mySubImage : ", mySubImage);
+//        new DownloadImageTask((ImageView) view.findViewById(R.id.mySubImage))
+//                .execute(mySubImage);
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
--- app/src/main/java/com/cst2335/ticketmaster/WishList.java
+++ app/src/main/java/com/cst2335/ticketmaster/WishList.java
@@ -1,25 +1,36 @@
 package com.cst2335.ticketmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
 import java.util.ArrayList;
-import java.util.concurrent.ExecutionException;
 
 public class WishList extends AppCompatActivity {
     // testtest
     ArrayList<Events> events = new ArrayList<>();
     WishListAdapter wishListAdapter;
+    EventOpener helper;
+    SQLiteDatabase db;
+    Cursor cursor;
+    WishFragment wishFm;
+    Bundle bundle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,17 +39,78 @@ public class WishList extends AppCompatActivity {
         String resultText;
         ListView listView = findViewById(R.id.myWishList);
         listView.setAdapter(wishListAdapter = new WishListAdapter());
+        Button btn1 = findViewById(R.id.wish_btn1);
 
-        try {
-            resultText = new WishTask().execute().get();
-            Log.i("test", "GOOOD!!");
-            listjsonParser(resultText, events);
-            wishListAdapter.notifyDataSetChanged();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
+        helper = new EventOpener(this);
+        db = helper.getWritableDatabase();
+        cursor = db.rawQuery("SELECT * FROM " + helper.TABLE_NAME + ";", null);
+
+        int idIndex = cursor.getColumnIndex("_id");
+        int nameIndex = cursor.getColumnIndex(helper.COL_NAME);
+        int imgUrlIndex = cursor.getColumnIndex(helper.COL_IMG_URL);
+        int cityIndex = cursor.getColumnIndex(helper.COL_LOCATION);
+        int startDateIndex = cursor.getColumnIndex(helper.COL_DATE);
+
+        // cursor is pointing to row - 1
+        // keep looping until no more data
+        while (cursor.moveToNext()) {
+            Long id = cursor.getLong(idIndex);
+            String name = cursor.getString(nameIndex);
+            String imgUrl = cursor.getString(imgUrlIndex);
+            String city = cursor.getString(cityIndex);
+            String startDate = cursor.getString(startDateIndex);
+
+            events.add(new Events(name, "", id + "", "", imgUrl, startDate, "", city));
+
         }
+        listView.setOnItemClickListener((p, b, position, id) -> {
+            Events clickedEvent = events.get(position);
+            wishFm = new WishFragment();
+            bundle = new Bundle();
+            bundle.putString("mySubName",clickedEvent.getName());
+            bundle.putString("mySubImage",clickedEvent.getImgUrl());
+            wishFm.setArguments(bundle);
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.myFrame, wishFm).commit();
+        });
+
+
+//        Log.i("isPhone","false");
+
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ContentValues newRow = new ContentValues();
+                for (int i = 0; i < events.size(); i++) {
+
+                    newRow.put(helper.COL_NAME, events.get(i).getName());
+                    newRow.put(helper.COL_TYPE, "F");
+                    newRow.put(helper.COL_URL, events.get(i).getUrl());
+                    newRow.put(helper.COL_IMG_URL, events.get(i).getImgUrl());
+                    newRow.put(helper.COL_DATE, events.get(i).getStartDate());
+                    newRow.put(helper.COL_STATUS, events.get(i).getStatus());
+                    newRow.put(helper.COL_LOCATION, events.get(i).getCity());
+                    newRow.put(helper.COL_CATEGORY, "COL_CATEGORY");
+                    newRow.put(helper.COL_PRICE, "COL_PRICE");
+                    newRow.put(helper.COL_DETAILS, "COL_DETAILS");
+//                    long id = db.insert(helper.TABLE_NAME,null,newRow);
+//                    Log.i("id",id+"");
+                }
+            }
+        });
+
+//        try {
+//            resultText = new WishTask().execute().get();
+//            Log.i("test", "GOOOD!!");
+//            listjsonParser(resultText, events);
+//            wishListAdapter.notifyDataSetChanged();
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        } catch (ExecutionException e) {
+//            e.printStackTrace();
+//        }
     }
 
     public ArrayList listjsonParser(String jsonString, ArrayList<Events> tickets) {
@@ -107,7 +179,7 @@ public class WishList extends AppCompatActivity {
             Events ticket = (Events) getItem(position);
             new DownloadImageTask((ImageView) newView.findViewById(R.id.myWishList_image))
                     .execute(ticket.getImgUrl());
-            textView.setText(ticket.getName() + "/" + ticket.getStatus() + "/" + ticket.getStartDate());
+            textView.setText(ticket.getName() + "/" + ticket.getCity() + "/" + ticket.getStartDate());
             return newView;
         }
     }
