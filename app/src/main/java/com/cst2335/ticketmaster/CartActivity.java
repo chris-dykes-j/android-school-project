3db43d648a77768a0ee1ce0113b6dd14a286a355
kim00451 <kim00451@algonquinlive.com>
2022-08-03 18:23:50 -0400

    Taeyeon edit cart_activity(Edit)

--- app/src/main/java/com/cst2335/ticketmaster/CartActivity.java
+++ app/src/main/java/com/cst2335/ticketmaster/CartActivity.java
@@ -2,6 +2,65 @@ package com.cst2335.ticketmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.media.metrics.Event;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class CartActivity extends AppCompatActivity  {
+
+    ListView listView;
+    TextView textView;
+
+    EventOpener dbHelper;
+    SQLiteDatabase db = null;
+    Cursor cursor;
+    ArrayAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart);
+
+        listView = findViewById(R.id.cartList);
+        textView = findViewById(R.id.cartText);
+
+        dbHelper = new EventOpener(this);
+        db = dbHelper.getWritableDatabase();
+
+    }
+
+    public void listUpdate(View v){
+
+        cursor = db.rawQuery("SELECT * FROM Events", null);
+        startManagingCursor(cursor);
+
+        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_activated_1);
+
+        while (cursor.moveToNext()) {
+            adapter.add(cursor.getString(0));
+        }
+
+        listView.setAdapter(adapter);
+    }
+
+
+}
+
+/**package com.cst2335.ticketmaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -117,5 +176,8 @@ public class CartActivity extends AppCompatActivity  {
                 textView.setText(ticket.getName() + "/" + ticket.getStatus() + "/" + ticket.getStartDate());
                 return newView;
             }
+
+
         }
     }
+*/
