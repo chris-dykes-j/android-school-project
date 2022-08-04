1eeb1c73daf3e669af4487caec1343829e65d750
kim00451 <kim00451@algonquinlive.com>
2022-08-03 23:28:24 -0400

    Taeyeon edit cart_activity(Edit)

--- app/src/main/java/com/cst2335/ticketmaster/CartActivity.java
+++ app/src/main/java/com/cst2335/ticketmaster/CartActivity.java
@@ -2,59 +2,44 @@ package com.cst2335.ticketmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.media.metrics.Event;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
-import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 
-public class CartActivity extends AppCompatActivity  {
+public class CartActivity extends AppCompatActivity {
 
     ListView listView;
     TextView textView;
 
     EventOpener dbHelper;
     SQLiteDatabase db = null;
-    Cursor cursor;
-    ArrayAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
 
-        listView = findViewById(R.id.cartList);
-        textView = findViewById(R.id.cartText);
+        ListView listView = (ListView) findViewById(R.id.cartList);
 
-        dbHelper = new EventOpener(this);
+        dbHelper = new EventOpener( this);
         db = dbHelper.getWritableDatabase();
 
-    }
+        String sql = "select * from Events;";
+        Cursor c = db.rawQuery(sql, null);
+        String[] strs = new String[]{dbHelper.COL_NAME,dbHelper.COL_TYPE, dbHelper.COL_DATE, dbHelper.COL_PRICE };
+        int[] ints = new int[]{R.id.cartText};
 
-    public void listUpdate(View v){
-
-        cursor = db.rawQuery("SELECT * FROM Events", null);
-        startManagingCursor(cursor);
-
-        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_activated_1);
-
-        while (cursor.moveToNext()) {
-            adapter.add(cursor.getString(0));
-        }
+        SimpleCursorAdapter adapter = null;
+        adapter = new SimpleCursorAdapter(listView.getContext(), R.layout.my_cart_list, c, strs, ints, 0);
 
         listView.setAdapter(adapter);
+
     }
-
-
 }
 
 /**package com.cst2335.ticketmaster;
